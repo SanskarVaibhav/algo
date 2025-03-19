6a40972d7899b7f3ea60363799704955db4684ff
SanskarVaibhav <135600088+SanskarVaibhav@users.noreply.github.com>
2025-03-19 21:58:19 +0530

    Create Parenthesis Checker.java

---
+++ Parenthesis Checker.java
@@ -0,0 +1,85 @@
+/* 
+Parenthesis Checker
+Difficulty: EasyAccuracy: 28.56%Submissions: 652K+Points: 2
+Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']', verify the validity of the arrangement.
+An input string is valid if:
+
+         1. Open brackets must be closed by the same type of brackets.
+         2. Open brackets must be closed in the correct order.
+
+Examples :
+
+Input: s = "[{()}]"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "[()()]{}"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "([]"
+Output: False
+Explanation: The expression is not balanced as there is a missing ')' at the end.
+Input: s = "([{]})"
+Output: False
+Explanation: The expression is not balanced as there is a closing ']' before the closing '}'.
+Constraints:
+1 ≤ s.size() ≤ 106
+s[i] ∈ {'{', '}', '(', ')', '[', ']'}
+*/
+
+
+// https://www.geeksforgeeks.org/problems/parenthesis-checker2744/0/
+
+
+
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
+            String st = sc.next();
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char opening = stack.pop();
+                if ((c == ')' && opening != '(') ||
+                    (c == '}' && opening != '{') ||
+                    (c == ']' && opening != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
